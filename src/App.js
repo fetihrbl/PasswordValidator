44ce747abd2688602b6284531ee8d794425d5351
Fatih <fatihrbl@gmail.com>
2025-03-16 01:54:46 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import validator from "validator";
 import './App.css';
 
 function App() {
+
+  const [password, setpassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const validate = (value) => {
+    setpassword(value);
+    if (validator.isStrongPassword(value, {
+      minLength: 8, minLowercase: 1,
+      minUppercase: 1, minNumbers: 1, minSymbols: 1
+    })) {
+      setErrorMessage("✅ Strong Password");
+    } else {
+      setErrorMessage("❌ Weak Password - Try adding uppercase, numbers, and symbols!");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>🔒 Password Strength Checker</h2>
+
+      <input
+        type="password"
+        placeholder="Enter your password..."
+        value={password}
+        onChange={(e) => validate(e.target.value)}
+      />
+
+      <div className={`message ${errorMessage.includes("Strong") ? "strong" : "weak"}`}>
+        {errorMessage}
+      </div>
     </div>
   );
 }
